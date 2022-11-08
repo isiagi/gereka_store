53fcd38a4f636d13151d66d6e573b0d6507a4013
isiagi <isiagigeofrey0@gmail.com>
2022-11-08 23:29:15 +0300

    adds design banner

---
+++ src/components/room/Room.jsx
@@ -0,0 +1,19 @@
+import "./room.css";
+
+function Room() {
+  return (
+    <div className="room__container">
+      <div className="room__wrapper">
+        <h2>Make room for more friends, more family, more fun</h2>
+        <p>
+          In hac habitasse platea dictumst. Aenean lectus diam, tempus nec
+          luctus vel, ultricies non augue. Nulla ac libero sit amet orci laoreet
+          lobortis tristique quis.
+        </p>
+        <h3>View Items</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Room;
--- src/components/show/index.jsx
+++ src/components/show/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Room from '../room/Room'
 import Show from './Show'
 
 const Index = () => {
   return (
     <div>
         <Show heading={'ESSENTIAL ITEMS'}/>
+        <Room />
         <Show heading={'FEATURED ITEMS'}/>
     </div>
   )
